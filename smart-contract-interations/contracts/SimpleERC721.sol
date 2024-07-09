e2036dae029a25ea2cd876d1a5f25a970bd14272
Shaheen <riseandshaheen@gmail.com>
2024-07-09 14:36:30 +0530

    add: smart-contract-interactions

---
+++ smart-contract-interations/contracts/SimpleERC721.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SimpleERC721 is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+
+    constructor() ERC721("SimpleERC721", "SIM721") {}
+
+    function mintTo(address recipient) public returns (uint256) {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+}
